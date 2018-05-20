a17ef4462ab5916d4fe119036c14c0a32b305420
Jake Moore <jake.moorevr@gmail.com>
2018-05-19 21:40:16 -0300

    Added button that copies text inputed to text view

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String text;
+
+    EditText textInput;
+    Button button;
+    TextView displayText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textInput = (EditText) findViewById(R.id.editText);
+
+        button = (Button) findViewById(R.id.button);
+        displayText = (TextView) findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text = textInput.getText().toString();
+                displayText.setText(text);
+                showToast(text);
+            }
+        });
     }
+
+    private void showToast (String text) {
+        Toast.makeText(MainActivity.this, "Copying "+text, Toast.LENGTH_SHORT).show();
+    }
+
+
+
+
 }
